47ca91ee4c8a7ed98efdc91b4b5ce298da1a75f2
Ákos Kreutz <akoskreutze@example.com>
2021-07-12 18:55:54 +0200

    Minor Bugfixes and code complienece with ShellChecker and PEP8.

--- visualization_engine.sh
+++ visualization_engine.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-error_file="homebrew_dashboard.out"
-
 error() {
   timestamp=$(date '+%Y-%m-%d %H:%M:%S')
   echo -e "${1}"
@@ -12,7 +10,7 @@ error() {
 usage() {
   echo "Use -p or --password to set the admin password for Grafana. For security reasons this is mandatory. Example: -p 12345"
   echo "Use -d or --database to set the InfluxDB database name, if not set the default name will be used (homebrew). Example: -d BakerStreet"
-  echo "Use -s or --datasource to set the Grafana datasource name, if not set the default name will be used (homebrew). Example: -d BakerStreet"
+  echo "Use -s or --datasource to set the Grafana datasource name, if not set the default name will be used (InfluxDB). Example: -s Gerry"
   exit 2
 }
 
@@ -53,50 +51,38 @@ if [ -z ${database+x} ]; then
 fi
 
 if [ -z ${datasource+x} ]; then
-  database="homebrew"
+  datasource="InfluxDB"
 fi
 
 echo "Installing Prerequisites"
 apt update
-output=$(apt -y install python3 python3-pip python-apt wget curl 2>&1 > /dev/null)
-
-if [[ $? -ne 0 ]]; then
+if [[ output=$(apt -y install python3 python3-pip python-apt wget curl 2>&1 > /dev/null) -ne 0 ]]; then
   error "Unable to install prerequisites.\n${output}"
 fi
 
 echo "Adding InfluxDB Repository Key"
-output=$(wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add - 2>&1 > /dev/null)
-
-if [[ $? -ne 0 ]]; then
+if [[ output=$(wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add - 2>&1 > /dev/null) -ne 0 ]]; then
   error "Unable to add InfluxDB Repository Key.\n${output}"
 fi
 
 echo "Adding Grafana Repository Key"
-output=$(wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add - 2>&1 > /dev/null)
-
-if [[ $? -ne 0 ]]; then
+if [[ output=$(wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add - 2>&1 > /dev/null) -ne 0 ]]; then
   error "Unable to add Grafana Repository Key.\n${output}"
 fi
 
 echo "Adding InfluxDB Repository to the source list"
-output=$(echo "deb https://repos.influxdata.com/debian buster stable" | sudo tee /etc/apt/sources.list.d/influxdb.list 2>&1 > /dev/null)
-
-if [[ $? -ne 0 ]]; then
+if [[ output=$(echo "deb https://repos.influxdata.com/debian buster stable" | sudo tee /etc/apt/sources.list.d/influxdb.list 2>&1 > /dev/null) -ne 0 ]]; then
   error "Unable to add InfluxDB Repository to the source list.\n${output}"
 fi
 
 echo "Adding Grafana Repository to the source list"
-output=$(echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list)
-
-if [[ $? -ne 0 ]]; then
+if [[ output=$(echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list) -ne 0 ]]; then
   error "Unable to add Grafana Repository to the source list.\n${output}"
 fi
 
 echo "Installing InfluxDB & Grafana"
 apt update
-output=$(apt -y install influxdb grafana)
-
-if [[ $? -ne 0 ]]; then
+if [[ output=$(apt -y install influxdb grafana) -ne 0 ]]; then
   error "Unable to install InfluxDB & Grafana.\n${output}"
 fi
 
@@ -110,9 +96,7 @@ systemctl enable grafana-server
 systemctl start grafana-server
 
 echo "Installing Python Modules"
-output=$(pip3 install influxdb)
-
-if [[ $? -ne 0 ]]; then
+if [[ output=$(pip3 install influxdb) -ne 0 ]]; then
   error "Unable to install python modules.\n${output}"
 fi
 
@@ -130,3 +114,6 @@ output=$(curl -s -X PUT -H "Content-Type: application/json" -d '{
 if [[ $? -ne 0 ]]; then
   error "Unable to change Grafana admin user password.\n${output}"
 fi
+
+
+wget -cO - "https://github.com$(curl -s -L https://github.com/Akos-Kreutz/homebrew-visualization-engine/releases/latest | grep -o '/Akos-Kreutz/homebrew-visualization-engine/archive/refs/tags/.*.tar.gz')" > visualization-engine.tar.gz
